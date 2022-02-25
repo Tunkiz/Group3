becff5706a967d7ad2a436048485d03ce3be5ead
Mabunda Tunkis <68813933+Tunkiz@users.noreply.github.com>
2022-02-25 13:33:49 +0200

    Update BlockChain.java

--- BlockChain/BlockChain.java
+++ BlockChain/BlockChain.java
@@ -5,6 +5,9 @@
  */
 package blackchain;
 
+import java.security.MessageDigest;
+import java.util.Date;
+
 /**
  *
  * @author tunki
@@ -14,15 +17,55 @@ public class BlockChain {
     /**
      * @param args the command line arguments
      */
+        public String hash;
+	public String previousHash;
+	private final String data; //our data will be a simple message.
+	private final long timeStamp; //as number of milliseconds since 1/1/1970.
+
+	//Block Constructor.
+	public BlockChain(String data,String previousHash ) {
+		this.data = data;
+		this.previousHash = previousHash;
+		this.timeStamp = new Date().getTime();
+                this.hash = calculateHash();
+	}
+        
+        public String calculateHash() {
+	String calculatedhash = applySha256( 
+			previousHash +
+			Long.toString(timeStamp) +
+			data 
+			);
+	return calculatedhash;
+        }
+        public static String applySha256(String input){		
+		try {
+			MessageDigest digest = MessageDigest.getInstance("SHA-256");	        
+			//Applies sha256 to our input, 
+			byte[] hash = digest.digest(input.getBytes("UTF-8"));	        
+			StringBuilder hexString = new StringBuilder(); // This will contain hash as hexidecimal
+			for (int i = 0; i < hash.length; i++) {
+				String hex = Integer.toHexString(0xff & hash[i]);
+				if(hex.length() == 1) hexString.append('0');
+				hexString.append(hex);
+			}
+			return hexString.toString();
+		}
+		catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+        
     public static void main(String[] args) {
-                Block genesisBlock = new Block("0000", "0");
+                BlockChain genesisBlock = new BlockChain("medical_history,treatments_received, test_results, diagnoses, and medications_taken", "0");
 		
                 System.out.println("Hash for block 1 : " + genesisBlock.hash);
-		Block secondBlock = new Block("medical_history,treatments_received, test_results, diagnoses, and medications_taken",genesisBlock.hash);
+		BlockChain secondBlock = new BlockChain("medical_history,treatments_received, test_results, diagnoses, and medications_taken",genesisBlock.hash);
 		System.out.println("Hash for block 2 : " + secondBlock.hash);
 		
-		Block thirdBlock = new Block("medical_history,treatments_received, test_results, diagnoses, and medications_taken",secondBlock.hash);
+		BlockChain thirdBlock = new BlockChain("medical_history,treatments_received, test_results, diagnoses, and medications_taken",secondBlock.hash);
 		System.out.println("Hash for block 3 : " + thirdBlock.hash);
     }
+    
     
 }
